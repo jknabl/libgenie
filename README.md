ebd87872af278f4ef7ee7fb75d82a5bbd48cdc82
Jason Knabl <jason.knabl@gmail.com>
2013-05-01 19:52:54 -0400

    refactor, clarify readme

--- README.md
+++ README.md
@@ -1,9 +1,26 @@
 LIBGENIE
+--------
 
 Pull books from a (public) AMZN wishlist and see if they're available at a university library.
 
 The genie grabs an ISBN for each book in a given wishlist, then grabs alternate ISBNs using the LibraryThing API. Genie searches a chosen library by ISBN, does some scraping and parsing, and pops back a list of available books (library location, checkout status, and call number). 
 
+DEPENDENCIES
+------------
+
+See requirements.txt.
+
+Easiest way to get up and running would be to spin up a new venv, then:
+
+pip install -r requirements.txt
+
+RUN
+---
+
+python wishlist.py
+
 TODO
+----
 
 * Functional but too slow for production. Parallelize HTTP requests.
+* Make things pretty.
--- models.py
+++ models.py
@@ -8,9 +8,9 @@ from lxml import etree
 class Wishlist(object):
     def __init__(self, email=""):
         self.email = email
-        self.url = self.getURL(email)
-        self.books = self.getBooks(self.url)
-    def getURL(self, email=None):
+        self.url = self.get_url(email)
+        self.books = self.get_books(self.url)
+    def get_url(self, email=None):
         #Make HTTP request to amzn to get URL
         #for a user's wishlist
         if email is None:
@@ -20,42 +20,42 @@ class Wishlist(object):
         response = conn.getresponse()
         loc = response.getheader('location')
         return loc
-    def getBooks(self, url=None):
-        theBooks = []
+    def get_books(self, url=None):
+        the_books = []
         if url is None:
             url = self.url
-        urlContents = urllib.urlopen(url).read()
-        parsed = lxml.html.document_fromstring(urlContents)
+        url_contents = urllib.urlopen(url).read()
+        parsed = lxml.html.document_fromstring(url_contents)
         XMLitems = parsed.find_class("small productTitle")
         for a in parsed.cssselect('span.productTitle a'):
             regex = re.compile("/([\d\w-]+)\/dp\/([\d\w]+)")
-            newUrl = urlparse(a.get('href'))
-            match = regex.match(newUrl.path)
+            new_url = urlparse(a.get('href'))
+            match = regex.match(new_url.path)
             if match:
-                newBook = WishlistBook(a.text, newUrl.path.rsplit('/')[3], a.get('href'))
-                theBooks.append(newBook)
-        return theBooks
-    def printAllBooks(self):
+                new_book = WishlistBook(a.text, new_url.path.rsplit('/')[3], a.get('href'))
+                the_books.append(new_book)
+        return the_books
+    def print_all_books(self):
         for book in self.books:
-            book.printAttributes()
+            book.print_attributes()
 
 class WishlistBook(object):
     def __init__(self, title="", isbn="", url=""):
         self.title = title
         self.isbn = isbn
         self.url = url
-        self.altISBNs = self.getAltISBNS(isbn)
-    def printAttributes(self):
+        self.alt_isbns = self.get_alt_isbns(isbn)
+    def print_attributes(self):
         print "\nBook's title is %s" % self.title
         print "\nBook's ISBN is %s" % self.isbn
-        print "\nAlt ISBNs are %s" % self.altISBNs
+        print "\nAlt ISBNs are %s" % self.alt_isbns
         print "\nBook's url is %s" % self.url
-    def getAltISBNS(self, code):
+    def get_alt_isbns(self, code):
         alts = []
         lt = "www.librarything.com"
-        getAlts = httplib.HTTPConnection(lt)
-        getAlts.request("GET", "/api/thingISBN/%s" % code)
-        response = getAlts.getresponse()
+        get_alts = httplib.HTTPConnection(lt)
+        get_alts.request("GET", "/api/thingISBN/%s" % code)
+        response = get_alts.getresponse()
         parsed = etree.parse(response)
         alts = [x.text for x in parsed.findall('isbn')]
         return alts
@@ -63,28 +63,28 @@ class WishlistBook(object):
 class GenericSearch(object):
     def __init__(self, wishlist=[]):
         self.books = wishlist.books
-        self.isbns = [book.altISBNs for book in self.books] #must be a list of isbns
-    def singleSearch(self, book, isbns=None):
+        self.isbns = [book.alt_isbns for book in self.books] #must be a list of isbns
+    def single_search(self, book, isbns=None):
         if isbns==None:
             isbns = self.isbns
         record = []
-        finalRecord = {}
+        final_record = {}
         for isbn in isbns:
-            tempRecords = self.isbnSearch(isbn)
+            tempRecords = self.isbn_search(isbn)
             if tempRecords != None:
                 for r in tempRecords:
                     record.append(r)
         if len(record) > 0:
-            finalRecord[book.title] = record
+            final_record[book.title] = record
         else:
-            finalRecord[book.title] = [['Not available.']]
-        return finalRecord
-    def searchAll(self, books=None):
+            final_record[book.title] = [['Not available.']]
+        return final_record
+    def search_all(self, books=None):
         if books==None:
             books = self.books
         records = {}
         for book in books:
-            record = self.singleSearch(book, book.altISBNs)
+            record = self.single_search(book, book.alt_isbns)
             if len(record) > 0:
                 records.update(record)
         if len(records) > 0:
@@ -95,22 +95,22 @@ class GenericSearch(object):
 class CarletonSearch(GenericSearch):
     def __init__(self, wishlist=[]):
         GenericSearch.__init__(self, wishlist)
-    def isbnSearch(self, isbn):
+    def isbn_search(self, isbn):
         url = "http://catalogue.library.carleton.ca/search/i?SEARCH=%s" % isbn
         print url
         query = urllib.urlopen(url)
         record = []
-        innerRecord = []
+        inner_record = []
         count = 0
         parsed = lxml.html.document_fromstring(query.read())
         for td in parsed.cssselect('tr.bibItemsEntry td'):
             #strip tags & encode to ascii (from unicode)
             text = (re.sub('<[^<]+?>', '', td.text_content())).strip().encode('ascii', 'ignore')
-            innerRecord.append(text)
+            inner_record.append(text)
             if count == 2:
-                record.append(innerRecord)
+                record.append(inner_record)
                 count = 0
-                innerRecord = []
+                inner_record = []
             else:
                 count += 1
         if len(record) > 0:
@@ -121,26 +121,26 @@ class CarletonSearch(GenericSearch):
 class WesternSearch(GenericSearch):
     def __init__(self, wishlist=[]):
         GenericSearch.__init__(self, wishlist)
-    def isbnSearch(self, isbn):
+    def isbn_search(self, isbn):
         url = "http://uwo.summon.serialssolutions.com/search?t.isbn=%s" % isbn
         count = 0
         record = []
-        innerRecord = []
-        newUrl = None
+        inner_record = []
+        new_url = None
         try:
             query = urllib.urlopen(url)
             parsed = lxml.html.document_fromstring(query.read())
             for a in parsed.cssselect('div.previewDocumentTitle a'):
-                newUrl = a.get('href')
-                if newUrl != None:
-                    newQuery = urllib.urlopen(newUrl)
+                new_url = a.get('href')
+                if new_url != None:
+                    newQuery = urllib.urlopen(new_url)
                     newParsed = lxml.html.document_fromstring(newQuery.read())
                     for td in newParsed.cssselect('tr.bibItemsEntry td'):
                         text = (re.sub('<[^<]+?', '', td.text_content())).strip().encode('ascii', 'ignore')
-                        innerRecord.append(text)
+                        inner_record.append(text)
                         if count == 2:
-                            record.append(innerRecord)
-                            innerRecord = []
+                            record.append(inner_record)
+                            inner_record = []
                             count = 0
                         else:
                             count += 1
@@ -154,19 +154,19 @@ class WesternSearch(GenericSearch):
 class OttawaUSearch(GenericSearch):
     def __init__(self, wishlist=[]):
         GenericSearch.__init__(self, wishlist)
-    def isbnSearch(self, isbn):
+    def isbn_search(self, isbn):
         url = "http://orbis.uottawa.ca/search~S0/?searchtype=i&searcharg=%s&SORT=D" % isbn
         count = 0
         record = []
-        innerRecord = []
+        inner_record = []
         query = urllib.urlopen(url)
         parsed = lxml.html.document_fromstring(query.read())
         for td in parsed.cssselect('tr.bibItemsEntry td'):
             text = (re.sub('<[^<]+?>', '', td.text_content())).strip().encode('ascii', 'ignore')
-            innerRecord.append(text)
+            inner_record.append(text)
             if count == 2:
-                record.append(innerRecord)
-                innerRecord = []
+                record.append(inner_record)
+                inner_record = []
                 count = 0
             else:
                 count += 1
@@ -178,20 +178,20 @@ class OttawaUSearch(GenericSearch):
 class BrockSearch(GenericSearch):
     def __init__(self, wishlist=[]):
         GenericSearch.__init__(self, wishlist)
-    def isbnSearch(self, isbn):
+    def isbn_search(self, isbn):
         url = "http://catalogue.library.brocku.ca/search/a?searchtype=i&searcharg=%s&SORT=D" % isbn
         query = urllib.urlopen(url)
         record = []
-        innerRecord = []
+        inner_record = []
         count = 0
         parsed = lxml.html.document_fromstring(query.read())
         for td in parsed.cssselect('tr.bibItemsEntry td'):
             text = (re.sub('<[^<]+?>', '', td.text_content())).strip().encode('ascii', 'ignore')
-            innerRecord.append(text)
+            inner_record.append(text)
             if count == 2:
-                record.append(innerRecord)
+                record.append(inner_record)
                 count = 0
-                innerRecord = []
+                inner_record = []
             else:
                 count += 1
         if len(record) > 0:
@@ -202,18 +202,18 @@ class BrockSearch(GenericSearch):
 class QueensSearch(GenericSearch):
     def __init__(self, wishlist=[]):
         GenericSearch.__init__(self, wishlist)
-    def isbnSearch(self, isbn):
+    def isbn_search(self, isbn):
         url = "http://queensu.summon.serialssolutions.com/search?t.isbn=%s" %isbn
         query = urllib.urlopen(url)
         record = []
-        innerRecord = []
+        inner_record = []
         count = 0
-        newUrl = None
+        new_url = None
         parsed = lxml.html.document_fromstring(query.read())
         for a in parsed.cssselect('h3.resultTitle a'):
-            newUrl = a.get('href')
-        if newUrl != None:
-            query2 = urllib.urlopen(newUrl)
+            new_url = a.get('href')
+        if new_url != None:
+            query2 = urllib.urlopen(new_url)
         else:
             query2 = None
         if query2 != None:
@@ -222,10 +222,10 @@ class QueensSearch(GenericSearch):
                 if span.get('class') == "subfieldData":
                     text = (re.sub('<[^<]+?', '', span.text_content())).strip().encode('ascii', 'ignore')
                     cleanText = text.replace('Show on Floorplan', '')
-                    innerRecord.append(cleanText)
+                    inner_record.append(cleanText)
                     if count == 2:
-                        record.append(innerRecord)
-                        innerRecord = []
+                        record.append(inner_record)
+                        inner_record = []
                         count = 0
                     else:
                         count += 1
@@ -237,18 +237,18 @@ class QueensSearch(GenericSearch):
 class YorkSearch(GenericSearch):
     def __init__(self, wishlist=[]):
         GenericSearch.__init__(self, wishlist)
-    def isbnSearch(self, isbn):
+    def isbn_search(self, isbn):
         url = "https://www.library.yorku.ca/find/Search/Results?lookfor0%%5B%%5D=%s&lookfor0%%5B%%5D=&lookfor0%%5B%%5D=&type0%%5B%%5D=ISN&join1=AND&lookfor1%%5B%%5D=&lookfor1%%5B%%5D=&lookfor1%%5B%%5D=&type1%%5B%%5D=AllFields&join2=AND&lookfor2%%5B%%5D=&lookfor2%%5B%%5D=&lookfor2%%5B%%5D=&type2%%5B%%5D=AllFields&daterange%%5B%%5D=publishDate&publishDatefrom=&publishDateto=" % isbn
-        newUrl = None
+        new_url = None
         query = urllib.urlopen(url)
         record = []
-        innerRecord = []
+        inner_record = []
         count = 0
         parsed = lxml.html.document_fromstring(query.read())
         for a in parsed.cssselect('div.resultItemLine1 a'):
-            newUrl = a.get('href')
-        if newUrl != None:
-            query2 = urllib.urlopen(newUrl)
+            new_url = a.get('href')
+        if new_url != None:
+            query2 = urllib.urlopen(new_url)
         else:
             query2 = None
         if query2 != None:
@@ -257,10 +257,10 @@ class YorkSearch(GenericSearch):
             for td in parsed2.cssselect('table.itemStatus tr td'):
                 if (td.get('class') == "locationColumn") or (td.get('class') == "callnumColumn") or (td.get('class')=="statusColumn"):
                     text = (re.sub('<[^<]+?', '', td.text_content())).strip().encode('ascii', 'ignore')
-                    innerRecord.append(text)
+                    inner_record.append(text)
                     if count == 2:
-                        record.append(innerRecord)
-                        innerRecord = []
+                        record.append(inner_record)
+                        inner_record = []
                         count = 0
                     else:
                         count += 1
@@ -272,19 +272,19 @@ class YorkSearch(GenericSearch):
 class WindsorSearch(GenericSearch):
     def __init__(self, wishlist=[]):
         GenericSearch.__init__(self, wishlist)
-    def isbnSearch(self, isbn):
+    def isbn_search(self, isbn):
         url = "http://windsor.concat.ca/eg/opac/results?query=%s&qtype=keyword&locg=106&detail_record_view=1&_adv=1&page=0&sort=" % isbn
         query = urllib.urlopen(url)
         record = []
-        innerRecord = []
+        inner_record = []
         count = 0
         parsed = lxml.html.document_fromstring(query.read())
         for td in parsed.cssselect('table.result_holdings_table tbody tr td'):
             text = (re.sub('<[^<]+?', '', td.text_content())).strip().encode('ascii', 'ignore')
-            innerRecord.append(text)
+            inner_record.append(text)
             if count == 3:
-                record.append(innerRecord)
-                innerRecord = []
+                record.append(inner_record)
+                inner_record = []
                 count = 0
             else:
                 count += 1
@@ -296,11 +296,11 @@ class WindsorSearch(GenericSearch):
 class LakeheadSearch(GenericSearch):
     def __init__(self, wishlist=[]):
         GenericSearch.__init__(self, wishlist)
-    def isbnSearch(self, isbn):
+    def isbn_search(self, isbn):
         print "ISBN IS %s\n" % isbn
         url = "http://inukshuk.lakeheadu.ca:7008/vwebv/search?searchArg1=%s&argType1=all&searchCode1=ISBN&recCount=10&searchType=2&page.search.search.button=Search" % isbn
         record = []
-        innerRecord = []
+        inner_record = []
         count = 0
         try:
             query = urllib.urlopen(url)
@@ -309,10 +309,10 @@ class LakeheadSearch(GenericSearch):
                 if div.get('class')=="fieldData":
                     text = (re.sub('<[^<]+?', '', div.text_content())).strip().encode('ascii', 'ignore')
                     print text
-                    innerRecord.append(text)
+                    inner_record.append(text)
                     if count == 2:
-                        record.append(innerRecord)
-                        innerRecord = []
+                        record.append(inner_record)
+                        inner_record = []
                         count = 0
                     else:
                         count += 1
@@ -327,10 +327,10 @@ class LakeheadSearch(GenericSearch):
 class RyersonSearch(GenericSearch):
     def __init__(self, wishlist=[]):
         GenericSearch.__init__(self, wishlist)
-    def isbnSearch(self, isbn):
+    def isbn_search(self, isbn):
         url = "http://ryerson.summon.serialssolutions.com/search?t.isbn=%s" % isbn
         record = []
-        innerRecord = []
+        inner_record = []
         count = 0
         query = urllib.urlopen(url)
         parsed = lxml.html.document_fromstring(query.read())
@@ -340,10 +340,10 @@ class RyersonSearch(GenericSearch):
             parsed2 = lxml.html.document_fromstring(query2.read())
             for td in parsed2.cssselect('tr.bibItemsEntry td'):
                 text = (re.sub('<[^<]+?', '', td.text_content())).strip().encode('ascii', 'ignore')
-                innerRecord.append(text)
+                inner_record.append(text)
                 if count == 2:
-                    record.append(innerRecord)
-                    innerRecord = []
+                    record.append(inner_record)
+                    inner_record = []
                     count = 0
                 else:
                     count += 1
@@ -355,10 +355,10 @@ class RyersonSearch(GenericSearch):
 class OCADSearch(GenericSearch):
     def __init__(self, wishlist=[]):
         GenericSearch.__init__(self, wishlist)
-    def isbnSearch(self, isbn):
+    def isbn_search(self, isbn):
         url = "http://ipac.ocad.on.ca/ipac20/ipac.jsp?menu=search&&index=ISBNEX&term=%s" % isbn
         record = []
-        innerRecord = []
+        inner_record = []
         query = urllib.urlopen(url)
         parsed = lxml.html.document_fromstring(query.read())
         for tr in parsed.cssselect('table.tableBackground tr'):
@@ -369,11 +369,11 @@ class OCADSearch(GenericSearch):
                     if text == "Item Information":
                         pass
                     else:
-                        innerRecord.append(text)
-                print "innerRecord is: %s\n" % innerRecord
-            if len(innerRecord) > 0:
-                record.append(innerRecord)
-            innerRecord = []
+                        inner_record.append(text)
+                print "inner_record is: %s\n" % inner_record
+            if len(inner_record) > 0:
+                record.append(inner_record)
+            inner_record = []
         if len(record) > 0:
             print "Record is %s\n" % record
             return record
@@ -384,7 +384,7 @@ class OCADSearch(GenericSearch):
 def main():
     wishlist = Wishlist("jason@wanderingrocks.com")
     search = CarletonSearch(wishlist)
-    results = search.searchAll()
+    results = search.search_all()
     print results
 if __name__=='__main__':
     main()
